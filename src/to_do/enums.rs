291e6bdbba3874af00eb12c737f6a3870b552381
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-07-31 21:02:42 -0300

    to_do Part 01

--- src/to_do/enums.rs
+++ src/to_do/enums.rs
@@ -1,13 +1,31 @@
+//use std::fmt;
+
 pub enum TaskStatus {
     DONE,
-    PENDING
+    PENDING,
 }
 
 impl TaskStatus {
     pub fn stringify(&self) -> String {
         match &self {
-            &Self::DONE => {"DONE".to_string()},
-            &Self::PENDING => {"PENDING".to_string()}
+            &Self::DONE => "DONE".to_string(),
+            &Self::PENDING => "PENDING".to_string(),
         }
     }
 }
+
+/* impl fmt::Display for TaskStatus {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match &self {
+            &Self::DONE => {write!(f, "DONE")},
+            &Self::PENDING => {write!(f, "PENDING")}
+        }
+    }
+} */
+
+//println!("{}", TaskStatus::DONE);
+//println!("{}", TaskStatus::PENDING);
+
+/* let outcome = TaskStatus::DONE.to_string;
+println!("{}", outcome);
+ */
