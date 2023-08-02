50f1c7cb3e3f387a19cbaebd1dc00868a005ed72
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-08-02 20:22:34 -0300

    Reading and writing JSON files

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+mod state;
 mod to_do;
+
+use serde_json::value::Value;
+use serde_json::{json, Map};
+use state::{read_file, write_to_file};
+use std::env;
+
 use to_do::enums::TaskStatus;
 use to_do::to_do_factory;
 use to_do::ItemTypes;
@@ -19,4 +26,17 @@ fn main() {
             item.set_to_done(&item.super_struct.title);
         }
     }
+
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 3 {
+        eprintln!("Usage: ./program <status> <title>");
+        return;
+    }
+    let status: &String = &args[1];
+    let title: &String = &args[2];
+    let mut state: Map<String, Value> = read_file("./state.json");
+    println!("Before operation: {:?}", state);
+    state.insert(title.to_string(), json!(status));
+    println!("After operation: {:?}", state);
+    write_to_file("./state.json", &mut state);
 }
