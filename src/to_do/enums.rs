73dca2b508c5aec07e6ccf352907518decd1cb6d
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-08-03 20:21:05 -0300

    Revisiting traits to interact with the json file

--- src/to_do/enums.rs
+++ src/to_do/enums.rs
@@ -29,3 +29,20 @@ pub enum TaskStatus {
 /* let outcome = TaskStatus::DONE.to_string;
 println!("{}", outcome);
  */
+
+impl TaskStatus {
+    pub fn stringify(&self) -> String {
+        match &self {
+            &Self::DONE => "DONE".to_string(),
+            &Self::PENDING => "PENDING".to_string(),
+        }
+    }
+
+    pub fn from_string(input_string: String) -> Self {
+        match input_string.as_str() {
+            "DONE" => TaskStatus::DONE,
+            "PENDING" => TaskStatus::PENDING,
+            _ => panic!("input {} not supported", input_string),
+        }
+    }
+}
