b29d76688f93cacb0120020dd6669e4aabdf9331
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-08-08 14:18:33 -0300

    Init Project with Axum

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,5 @@
-use actix_web::{web, App, HttpRequest, HttpServer, Responder};
 
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", name)
-}
+fn main()  {
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(greet))
-            .route("/{name}", web::get().to(greet))
-            .route("/say/hello", web::get().to(|| async { "Hello Again!" }))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
+    println!("Hello World..!")
 }
