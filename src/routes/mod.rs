9d346e08a7b58c49c77b0370ab94395b46b09765
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-08-20 12:27:12 -0300

    Partial Updates

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,16 +1,18 @@
 mod create_task;
 mod get_tasks;
 mod hello_world;
+mod partial_update_task;
 mod update_tasks;
 
 use axum::{
-    routing::{get, post, put},
+    routing::{get, patch, post, put},
     Extension, Router,
 };
 
 use create_task::create_task;
 use get_tasks::{get_all_tasks, get_one_task};
 use hello_world::hello_world;
+use partial_update_task::partial_update;
 use sea_orm::DatabaseConnection;
 use update_tasks::atomic_update;
 
@@ -21,5 +23,6 @@ pub async fn create_routes(database: DatabaseConnection) -> Router {
         .route("/tasks", get(get_all_tasks))
         .route("/tasks/:task_id", get(get_one_task))
         .route("/tasks/:task_id", put(atomic_update))
+        .route("/tasks/:task_id", patch(partial_update))
         .layer(Extension(database))
 }
