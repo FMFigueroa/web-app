8c3865860b61496ec9d5247d5aa3b3a2509b9ac6
FMFigueroa <felixmanuelfigueroa@gmail.com>
2023-09-04 21:41:13 -0300

    Update Delete Task

--- src/routes/delete_task.rs
+++ src/routes/delete_task.rs
@@ -1,61 +1,29 @@
-use crate::database::tasks::{self, Entity as Tasks};
-use axum::{
-    extract::{Path, Query, State},
-    http::StatusCode,
+use crate::{
+    database::users::Model,
+    queires::task_queries::{find_task_by_id, save_active_task},
+    utils::app_error::AppError,
 };
-use sea_orm::{ColumnTrait, DatabaseConnection, EntityTrait, IntoActiveModel, QueryFilter, Set};
-use serde::Deserialize;
-
-#[derive(Deserialize)]
-pub struct QueryParams {
-    soft: bool,
-}
+use axum::{
+    extract::{Path, State},
+    Extension,
+};
+use chrono::Utc;
+use sea_orm::{DatabaseConnection, IntoActiveModel, Set};
 
 pub async fn delete_task(
+    State(db): State<DatabaseConnection>,
     Path(task_id): Path<i32>,
-    State(database): State<DatabaseConnection>,
-    Query(query_params): Query<QueryParams>,
-) -> Result<(), StatusCode> {
-    // let task = if let Some(task) = Tasks::find_by_id(task_id)
-    //     .one(&database)
-    //     .await
-    //     .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
-    // {
-    //     task.into_active_model()
-    // } else {
-    //     return Err(StatusCode::NOT_FOUND);
-    // };
+    Extension(user): Extension<Model>,
+) -> Result<(), AppError> {
+    let mut task = find_task_by_id(&db, task_id, user.id)
+        .await?
+        .into_active_model();
 
-    // Tasks::delete(task)
-    //     .exec(&database)
-    //     .await
-    //     .map_err(|_error| StatusCode::INTERNAL_SERVER_ERROR)?;
+    let now = Utc::now();
 
-    if query_params.soft {
-        let mut task = if let Some(task) = Tasks::find_by_id(task_id)
-            .one(&database)
-            .await
-            .map_err(|_| StatusCode::INTERNAL_SERVER_ERROR)?
-        {
-            task.into_active_model()
-        } else {
-            return Err(StatusCode::NOT_FOUND);
-        };
+    task.deleted_at = Set(Some(now.into()));
 
-        let now = chrono::Utc::now();
-
-        task.deleted_at = Set(Some(now.into()));
-        Tasks::update(task)
-            .exec(&database)
-            .await
-            .map_err(|_error| StatusCode::INTERNAL_SERVER_ERROR)?;
-    } else {
-        Tasks::delete_many()
-            .filter(tasks::Column::Id.eq(task_id))
-            .exec(&database)
-            .await
-            .map_err(|_error| StatusCode::INTERNAL_SERVER_ERROR)?;
-    }
+    save_active_task(&db, task).await?;
 
     Ok(())
 }
